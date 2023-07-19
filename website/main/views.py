23fb665869ce510a46ab31fa8404743456c9fd9d
nikbukhtoyarov <nik_bukhtoyarov@mail.ru>
2023-07-19 15:49:22 +0300

    Initial commit

---
+++ website/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'hello', '123'],  # Создание списка
+        'obj':{
+            'name': 'Nick',
+            'age': '23',
+            'status': 'student'
+        }
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
