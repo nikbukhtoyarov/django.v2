025f63f61bb59aba617f1402f1f37b36bd5c57d2
nikbukhtoyarov <nik_bukhtoyarov@mail.ru>
2023-08-15 14:51:07 +0300

    Change sqlite to Postgres

--- website/website/settings.py
+++ website/website/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'nikita_bukhtoyarov',
+        'PASSWORD': 'Walhala2310',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
